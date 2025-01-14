06befea6e9f607a5e9086adf8234c7e4b4b31f43
jangjinseong <hn04223@gmail.com>
2025-01-14 09:48:29 +0900

    2025/01/14 기본틀

---
+++ src/main/java/com/green/jobdone/JobDoneApplication.java
@@ -0,0 +1,13 @@
+package com.green.jobdone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobDoneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobDoneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/green/jobdone/JobDoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.green.jobdone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobDoneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
