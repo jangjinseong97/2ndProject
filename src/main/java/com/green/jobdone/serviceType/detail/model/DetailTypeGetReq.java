7bbb6014de76d2d58a600f6317dac24d7da696e4
yoonseoq <sysooo18@naver.com>
2025-01-17 20:35:29 +0900

    250117 2030

--- src/main/java/com/green/jobdone/serviceType/detail/model/DetailTypeGetReq.java
+++ src/main/java/com/green/jobdone/serviceType/detail/model/DetailTypeGetReq.java
@@ -1,7 +1,11 @@
 package com.green.jobdone.serviceType.detail.model;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class DetailTypeGetReq {
     @Schema(title = "카페 아이디", example = "1", requiredMode = Schema.RequiredMode.REQUIRED)
     private long detailTypeId;
--- src/main/java/com/green/jobdone/serviceType/detail/model/DetailTypeGetRes.java
+++ src/main/java/com/green/jobdone/serviceType/detail/model/DetailTypeGetRes.java
@@ -1,4 +1,9 @@
 package com.green.jobdone.serviceType.detail.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class DetailTypeGetRes {
 }
