b90f76a3273a6fd9897b5a32565ff4930cbba4d3
home <hn04223@gmail.com>
2025-01-18 17:05:37 +0900

    25/01/18 집

--- src/main/java/com/green/jobdone/service/ServiceController.java
+++ src/main/java/com/green/jobdone/service/ServiceController.java
@@ -1,16 +1,17 @@
 package com.green.jobdone.service;
 
 import com.green.jobdone.common.model.ResultResponse;
+import com.green.jobdone.service.model.ServiceGetReq;
+import com.green.jobdone.service.model.ServiceGetRes;
 import com.green.jobdone.service.model.ServicePostReq;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springdoc.core.annotations.ParameterObject;
+import org.springframework.web.bind.annotation.*;
 
 import javax.lang.model.element.Name;
+import java.util.List;
 
 @RequestMapping("service")
 @RequiredArgsConstructor
@@ -25,6 +26,14 @@ public class ServiceController {
         return ResultResponse.<Integer>builder()
                 .resultData(res)
                 .resultMessage(res == 0 ? "예약 실패" : "예약 성공")
+                .build();
+    }
+
+    public ResultResponse<List<ServiceGetRes>> getService(@ParameterObject @ModelAttribute ServiceGetReq p){
+        List<ServiceGetRes> res = serviceService.getService(p);
+        return ResultResponse.<List<ServiceGetRes>>builder()
+                .resultMessage("조회 완료")
+                .resultData(res)
                 .build();
     }
 
