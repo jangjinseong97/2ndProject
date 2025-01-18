a07eeeb5332d2ebed78ccafab0cde83d06c67d85
home <hn04223@gmail.com>
2025-01-18 16:54:32 +0900

    25/01/18 집

--- src/main/java/com/green/jobdone/service/ServiceMapper.java
+++ src/main/java/com/green/jobdone/service/ServiceMapper.java
@@ -1,11 +1,16 @@
 package com.green.jobdone.service;
 
+import com.green.jobdone.service.model.ServiceGetReq;
+import com.green.jobdone.service.model.ServiceGetRes;
 import com.green.jobdone.service.model.ServicePostReq;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 @Mapper
 public interface ServiceMapper {
 
     int insService(ServicePostReq p);
     int insServiceDetail(ServicePostReq p);
+    List<ServiceGetRes> GetServiceFlow(ServiceGetReq p);
 }
--- src/main/java/com/green/jobdone/service/model/ServiceGetRes.java
+++ src/main/java/com/green/jobdone/service/model/ServiceGetRes.java
@@ -4,8 +4,27 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import java.util.List;
+
 @Getter
 @Setter
 @ToString
 public class ServiceGetRes {
+    private String userName;
+    private String businessName;
+    private String productName;
+    private String startTime;
+    private String endTime;
+    private String mStartTime;
+    private String mEndTime;
+    private int allow;
+    private String userPhone;
+    private int price;
+    private int completed;
+    private long serviceId;
+    private String address;
+    private String comment;
+    private String addComment;
+    private List<String> businessPhone;
+    private List<ServiceOptionDto> options;
 }
