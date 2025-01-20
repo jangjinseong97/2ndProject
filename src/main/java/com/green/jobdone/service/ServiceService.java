a0fa0d2d6550865634993f5462b804fa5bdabedf
jangjinseong <hn04223@gmail.com>
2025-01-20 09:33:26 +0900

    25/01/20

--- src/main/java/com/green/jobdone/service/ServiceService.java
+++ src/main/java/com/green/jobdone/service/ServiceService.java
@@ -1,10 +1,14 @@
 package com.green.jobdone.service;
 
+import com.green.jobdone.service.model.ServiceGetReq;
+import com.green.jobdone.service.model.ServiceGetRes;
 import com.green.jobdone.service.model.ServicePostReq;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
@@ -19,4 +23,11 @@ public class ServiceService {
         return res;
     }
 
+    public List<ServiceGetRes> getService(ServiceGetReq p){
+        if(p == null || p.getUserId()==null && p.getBusinessId()==null){
+            return new ArrayList<>();
+        }
+        List<ServiceGetRes> res = serviceMapper.GetServiceFlow(p);
+        return res;
+    }
 }
--- src/main/java/com/green/jobdone/service/model/ServiceGetRes.java
+++ src/main/java/com/green/jobdone/service/model/ServiceGetRes.java
@@ -4,8 +4,27 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import java.util.List;
+
 @Getter
 @Setter
 @ToString
 public class ServiceGetRes {
+    private long serviceId;
+    private String userName;
+    private String businessName;
+    private String productName;
+    private String userPhone;
+    private String address;
+    private int price;
+    private int completed;
+    private String comment;
+    private String addComment;
+    private String startTime;
+    private String endTime;
+    private String mStartTime;
+    private String mEndTime;
+    private int allow;
+    private List<String> businessPhone;
+    private List<ServiceOptionDto> options;
 }
