26faf2007a50320c3821e1fd972daea42890eb4b
yoonseoq <sysooo18@naver.com>
2025-01-20 14:01:40 +0900

    250120 00

--- src/main/java/com/green/jobdone/JobDoneApplication.java
+++ src/main/java/com/green/jobdone/JobDoneApplication.java
@@ -2,8 +2,10 @@ package com.green.jobdone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class JobDoneApplication {
 
     public static void main(String[] args) {
