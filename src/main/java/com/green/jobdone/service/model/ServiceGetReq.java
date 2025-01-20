bc04eb9671a8900761235487a65af96bb1ebb053
jangjinseong <hn04223@gmail.com>
2025-01-20 14:15:14 +0900

    ㅇㅇ

---
+++ src/main/java/com/green/jobdone/service/model/ServiceGetReq.java
@@ -0,0 +1,22 @@
+package com.green.jobdone.service.model;
+import com.green.jobdone.common.model.Paging;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.web.bind.annotation.BindParam;
+@Getter
+@Setter
+@ToString(callSuper = true)
+public class ServiceGetReq extends Paging {
+    @Schema(name = "user_id")
+    private Long userId;
+    @Schema(name = "business_id")
+    private Long businessId;
+    public ServiceGetReq(Integer page, Integer size,
+                         @BindParam("user_id")Long userId, @BindParam("business_id")Long businessId)  {
+        super(page, size);
+        this.userId = userId;
+        this.businessId = businessId;
+    }
+}
