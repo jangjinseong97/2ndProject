bb27210cf63ad7d1c1524e0f454dcfebadc282c6
jangjinseong <hn04223@gmail.com>
2025-01-21 12:45:51 +0900

    ㅇㅇ

--- src/main/java/com/green/jobdone/room/model/RoomGetRes.java
+++ src/main/java/com/green/jobdone/room/model/RoomGetRes.java
@@ -1,4 +1,15 @@
 package com.green.jobdone.room.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class RoomGetRes {
+    private long roomId;
+    private Long chatId;
+    private String recentlyChat;
+    private String roomCreatedAt;
+    private String title;
+
 }
