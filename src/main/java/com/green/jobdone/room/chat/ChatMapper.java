acf71636dadc29a9c526262ca19b474cbb0752b7
home <hn04223@gmail.com>
2025-01-27 16:26:54 +0900

    25/01/27 get 수정 + chat일부

--- src/main/java/com/green/jobdone/room/chat/ChatMapper.java
+++ src/main/java/com/green/jobdone/room/chat/ChatMapper.java
@@ -1,11 +1,16 @@
 package com.green.jobdone.room.chat;
 
+import com.green.jobdone.room.chat.model.ChatGetReq;
+import com.green.jobdone.room.chat.model.ChatGetRes;
 import com.green.jobdone.room.chat.model.ChatPicDto;
 import com.green.jobdone.room.chat.model.ChatPostReq;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 @Mapper
 public interface ChatMapper {
     int insChat(ChatPostReq p);
     int insChatPic(ChatPicDto p);
+    List<ChatGetRes> selRoomChat(ChatGetReq p);
 }
