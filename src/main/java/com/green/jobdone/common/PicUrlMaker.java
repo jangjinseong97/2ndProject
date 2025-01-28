a6895e6559bdfb05f2ab2bb05f78553c03364fc5
seok <sysooo18@naver.com>
2025-01-28 18:11:27 +0900

    250128 포폴 조회 완료/ 다만 포폴리스트 하려면 포트폴리오 테이블에 썸네일 컬럼 만들던가 포폴 사진 테이블에서 하나 지정해야함. 프론트한테 물어보기

--- src/main/java/com/green/jobdone/common/PicUrlMaker.java
+++ src/main/java/com/green/jobdone/common/PicUrlMaker.java
@@ -2,7 +2,7 @@ package com.green.jobdone.common;
 
 public class PicUrlMaker {
     public static String makePicUrl(long businessId, String picName) {
-        return String.format("/pic/cafe/%d/%s", businessId, picName);
+        return String.format("/pic/business/%d/%s", businessId, picName);
     }
 
     public static String makePicUrl(long cafeId, long menuId, String picName) {
