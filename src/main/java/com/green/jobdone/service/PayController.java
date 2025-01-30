36e480ac08002e6e8f52c95bfdc45cda224f9080
home <hn04223@gmail.com>
2025-01-30 18:20:23 +0900

    25/01/30 업데이트문 보안성 post수정 카카오페이api(수정과 추가 필요)

--- src/main/java/com/green/jobdone/service/PayController.java
+++ src/main/java/com/green/jobdone/service/PayController.java
@@ -16,26 +16,27 @@ public class PayController {
     private final PayService payService;
 
     @PostMapping("/ready")
-    public KakaoPayRedayRes startKakaoPay(@RequestParam Long serviceId){
+    public KakaoPayRedayRes startKakaoPay(@RequestParam Long serviceId) {
         log.info("serviceId : {}", serviceId);
         return payService.useKakaoPay(serviceId);
     }
+
     @PostMapping("/success")
     public ResponseEntity<KakaoPayRes> paySuccess(@RequestParam("pg_token") String pgToken,
                                                   @RequestParam Long serviceId,
-                                                  @RequestParam String tid){
+                                                  @RequestParam String tid) {
         KakaoPayRes kakaoPayRes = payService.payRes(pgToken, serviceId, tid);
 
         return new ResponseEntity<>(kakaoPayRes, HttpStatus.OK);
     }
 
     @GetMapping("/cancel")
-    public void cancel(){
-        throw new RuntimeException();
+    public ResponseEntity<String> cancel() {
+        return new ResponseEntity<>("Payment cancelled.", HttpStatus.OK);
     }
 
     @GetMapping("/fail")
-    public void fail(){
-        throw new RuntimeException();
+    public ResponseEntity<String> fail() {
+        return new ResponseEntity<>("Payment failed.", HttpStatus.OK);
     }
 }
