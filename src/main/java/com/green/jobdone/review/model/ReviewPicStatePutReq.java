00a2d3fe1ef1ac2b73b4fef14f1eedd7935d1c93
boyunsim <tlaqhdbs12@gmail.com>
2025-01-31 15:51:49 +0900

    2025-01-24 commit

--- src/main/java/com/green/jobdone/review/model/ReviewPicStatePutReq.java
+++ src/main/java/com/green/jobdone/review/model/ReviewPicStatePutReq.java
@@ -9,6 +9,8 @@ import lombok.ToString;
 @Setter
 @ToString
 public class ReviewPicStatePutReq {
-    @Schema(title = "리뷰 사진 pk", example = "1", requiredMode = Schema.RequiredMode.REQUIRED)
+    @Schema(title = "리뷰 사진 pk", example = "1",description = "리뷰 수정중 기존 사진 삭제시 리뷰사진 PK", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
     private long reviewPicId;
+    @Schema(title = "리뷰 pk", example = "1",description = "리뷰 수정중 취소시 데이터베이스 상태값 원래대로하기위해 리뷰 PK", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+    private long reviewId;
 }
