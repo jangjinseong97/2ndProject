0ee3172c099c0627180eb30319764271fc21750a
jangjinseong <hn04223@gmail.com>
2025-01-31 15:32:15 +0900

    2025/01/31

--- src/main/java/com/green/jobdone/service/PayController.java
+++ src/main/java/com/green/jobdone/service/PayController.java
@@ -3,6 +3,8 @@ package com.green.jobdone.service;
 import com.green.jobdone.service.model.Dto.KakaoPayReq;
 import com.green.jobdone.service.model.KakaoPayRedayRes;
 import com.green.jobdone.service.model.KakaoPayRes;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -13,16 +15,19 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/payment")
 @Slf4j
+@Tag(name = "카카오페이api")
 public class PayController {
     private final PayService payService;
 
     @PostMapping("/ready")
+    @Operation(summary = "결제 요청 보내기")
     public KakaoPayRedayRes startKakaoPay(@RequestParam Long serviceId) {
         log.info("serviceId : {}", serviceId);
         return payService.useKakaoPay(serviceId);
     }
 
     @PostMapping("/success")
+    @Operation(summary = "결제 성공 후 완료 메세지 보내기")
     public ResponseEntity<KakaoPayRes> paySuccess(@RequestBody KakaoPayReq p) {
         KakaoPayRes kakaoPayRes = payService.payRes(p.getPgToken(),p.getServiceId(),p.getTid());
 
