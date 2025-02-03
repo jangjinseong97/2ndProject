02fa8eb1f09e8526c5aae5077715e7daf3a6d68a
yoonseoq <sysooo18@naver.com>
2025-02-03 16:37:48 +0900

    250203 잠와죽겠다. 일단업체리스트 조회방법

--- src/main/java/com/green/jobdone/business/model/get/BusinessGetReq.java
+++ src/main/java/com/green/jobdone/business/model/get/BusinessGetReq.java
@@ -4,16 +4,21 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
-
 @Getter
 @Setter
 public class BusinessGetReq {
     @Schema(title = "대분류")
     private Long categoryId;
+
     @Schema(title = "소분류")
     private Long detailTypeId;
+
+    @Schema(title = "지역")
+    private Long regionId;
 
     @JsonIgnore
     private long signedUserId;
 
+    @Schema(title = "정렬 기준", description = "latest(최신순), rating(별점순), order(주문순)")
+    private String sortType;
 }
