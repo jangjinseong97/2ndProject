0ec3623163d4b29f9f1138e4980584f867d3998d
ParkJunHyeong <parkhung007@gmail.com>
2025-02-03 15:10:38 +0900

    토큰 필터 재설정, product get 완료

--- src/main/java/com/green/jobdone/config/security/WebSecurityConfig.java
+++ src/main/java/com/green/jobdone/config/security/WebSecurityConfig.java
@@ -38,10 +38,10 @@ public class WebSecurityConfig {
                 .formLogin(form -> form.disable()) // SSR을 사용하지 않기 때문에 폼 로그인 비활성화
                 .csrf(csrf -> csrf.disable()) // SSR이 아니면 CSRF 보호 필요 없음
                 .authorizeHttpRequests(req -> req
-//                           .requestMatchers(HttpMethod.GET,"/api/user").authenticated()
-//                           .requestMatchers(HttpMethod.PATCH,"/api/user/pic").authenticated()   //   나중에 대충 다 만들면 api 인증받을거 처리하기
-                           .requestMatchers("/api/swagger-ui/**", "/api/v3/api-docs/**").permitAll()
-//                        .requestMatchers("/api/like").hasRole(UserRole.USER.name()) // /api/like는 USER 역할을 가진 사용자만 접근 가능
+//                                .requestMatchers(HttpMethod.GET,"/api/user").authenticated()
+//                              .requestMatchers(HttpMethod.PATCH,"/api/user/pic").authenticated()   //   나중에 대충 다 만들면 api 인증받을거 처리하기
+                                .requestMatchers("/api/swagger-ui/**", "/api/v3/api-docs/**").permitAll()
+//                              .requestMatchers("/api/like").hasRole(UserRole.USER.name()) // /api/like는 USER 역할을 가진 사용자만 접근 가능
                         .anyRequest().permitAll() // 그 외의 모든 요청은 허용
                 )
                 .exceptionHandling(e -> e
