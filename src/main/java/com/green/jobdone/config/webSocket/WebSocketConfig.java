25c27a5eaee248d61d453343a2c65ca5d00b0f46
jangjinseong <hn04223@gmail.com>
2025-02-06 19:52:22 +0900

    2025/02/06 웹소켓 된듯?

--- src/main/java/com/green/jobdone/config/webSocket/WebSocketConfig.java
+++ src/main/java/com/green/jobdone/config/webSocket/WebSocketConfig.java
@@ -16,9 +16,16 @@ public class WebSocketConfig implements WebSocketConfigurer {
     public WebSocketConfig(ChatWebSocketHandler chatWebSocketHandler) {
         this.chatWebSocketHandler = chatWebSocketHandler;
     }
+
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(chatWebSocketHandler, "/chat").setAllowedOrigins("*");
+        registry.addHandler(chatWebSocketHandler, "/chat")
+                .setAllowedOrigins(
+                "http://192.168.0.195:5173",   // 개발용
+                "http://112.222.157.156:5224", // 외부 도메인
+                "http://192.168.0.77:5173",
+                "http://192.168.0.134:5173"
+        );
         //chat 이후에 어떤곳이라도 웹소케 헨들러 등록
     }
 }
